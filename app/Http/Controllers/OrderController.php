4a360ec0fc2a30d1a0bb78bebf5bedf66f5cd2c8
omar-abdelrady-pintar <omar.abdelrady@pintar.tech>
2025-01-28 02:32:00 +0200

    update 5 files

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,45 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Customer\OrderResource;
+use App\Models\Order;
 use App\Services\OrderService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class OrderController extends Controller {
-  protected OrderService $orderService;
-  public function __construct(
-    OrderService $orderService
-  ) {
-    $this->orderService = $orderService;
-  }
-  public function index(Request $request): JsonResponse {
-    $orders = $this->orderService->getFilteredOrders($request);
-    return response()->apiResponse($orders);
-  }
-
-  public function edit(Request $request, $id): JsonResponse {
-    DB::beginTransaction();
-    try {
-      $order = $this->orderService->getOrderById($id);
-      if (!$order) {
-        return response()->apiResponse(['error' => 'Order not found.'], 404);
-      }
-      $order = $this->orderService->updateOrder($request, $order);
-      DB::commit();
-      return response()->apiResponse($order);
-    } catch (\Exception $e) {
-      DB::rollBack();
-      return response()->apiResponse(['error' => $e->getMessage()], 500);
+class OrderController extends Controller
+{
+    protected OrderService $orderService;
+    public function __construct(
+        OrderService $orderService
+    ) {
+        $this->orderService = $orderService;
     }
-  }
-
-  public function destroy($id): JsonResponse {
-    $order = $this->orderService->getOrderById($id);
-    if (!$order) {
-      return response()->apiResponse(['error' => 'Order not found.'], 404);
+    public function index(Request $request): JsonResponse
+    {
+        $orders = $this->orderService->getFilteredOrders($request);
+        return response()->apiResponse($orders);
     }
-    $this->orderService->deleteOrder($order);
-    return response()->apiResponse(['message' => 'Order deleted successfully.']);
-  }
+
+    public function edit(Request $request, $id): JsonResponse
+    {
+        DB::beginTransaction();
+        try {
+            $order = $this->orderService->getOrderById($id);
+            if (!$order) {
+                return response()->apiResponse(['error' => 'Order not found.'], 404);
+            }
+            $order = $this->orderService->updateOrder($request, $order);
+            DB::commit();
+            return response()->apiResponse($order);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->apiResponse(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $order = $this->orderService->getOrderById($id);
+        if (!$order) {
+            return response()->apiResponse(['error' => 'Order not found.'], 404);
+        }
+        $this->orderService->deleteOrder($order);
+        return response()->apiResponse(['message' => 'Order deleted successfully.']);
+    }
+
+    public function getAllOrderStatus(): JsonResponse
+    {
+        $orderStatuses = $this->orderService->getAllOrderStatus();
+        return response()->apiResponse($orderStatuses);
+    }
+
+    public function updateOrderStatus(Request $request, Order $order): JsonResponse
+    {
+        $request->validate([
+            'order_status_id' => [
+                'required',
+                'in:' . implode(',', array_values($this->orderService->getAllOrderStatus())) ,
+            ],
+        ]);
+        $status = $request->input('order_status_id');
+        $this->orderService->updateOrderStatus($order, $status);
+        $order = OrderResource::make($order);
+        return response()->apiResponse($order);
+    }
 }
--- app/Http/Resources/Customer/OrderResource.php
+++ app/Http/Resources/Customer/OrderResource.php
@@ -5,101 +5,105 @@ namespace App\Http\Resources\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class OrderResource extends JsonResource {
-  /**
-   * Transform the resource into an array.
-   *
-   * @param Request $request
-   * @return array<string, mixed>
-   */
-  public function toArray($request): array {
-    if ($request->routeIs('orders.index', 'customer_app.orders.index')) {
-      return $this->transformForIndex();
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        if ($request->routeIs('orders.index', 'customer_app.orders.index', 'orders.updateOrderStatus')) {
+            return $this->transformForIndex();
+        }
+
+        return $this->transformForShow();
     }
 
-    return $this->transformForShow();
-  }
-
-  /**
-   * Transform the resource into an array for the index API.
-   *
-   * @return array<string, mixed>
-   */
-  public function transformForIndex(): array {
-    return [
-      'id' => $this->id,
-      'address' => [
-        'city' => $this->orderAddress->address->city,
-        'street' => $this->orderAddress->address->street,
-        'building_number' => $this->orderAddress->address->building_number,
-      ],
-      'user' => [
-        'name' => $this->user->name,
-      ],
-      'status' => $this->getStatusAttribute($this->order_status),
-      'created_at' => $this->created_at,
-      'total' => $this->total,
-    ];
-  }
-
-  /**
-   * Transform the resource into an array for the show API.
-   *
-   * @return array<string, mixed>
-   */
-  public function transformForShow(): array {
-    $orderItems = $this->orderProducts->map(function ($orderProduct) {
-      $product = $orderProduct->product;
-      $discountedPrice = $orderProduct->price * (1 - ($product->discount / 100));
-      $totalPrice = $discountedPrice * $orderProduct->quantity;
-      return [
-        'id' => $orderProduct->id,
-        'product_name' => $product->title,
-        'product_price' => $orderProduct->price,
-        'quantity' => $orderProduct->quantity,
-        'discount' => $product->discount,
-        'total_price' => $totalPrice,
-        'media' => $product->getMedia('products')->map(function ($media) {
-          return [
-            'original' => $media->getUrl(),
-            'thumb' => $media->getUrl('thumb'),
-            'medium' => $media->getUrl('medium'),
-            'large' => $media->getUrl('large'),
-          ];
-        }),
-      ];
-    });
-    $data = [
-      'id' => $this->id,
-      'order_type' => $this->order_type,
-      'total' => $this->total,
-      'status' => $this->getStatusAttribute($this->order_status),
-      'order_products' => $orderItems,
-      'user' => [
-        'id' => $this->user->id,
-        'name' => $this->user->name,
-        'email' => $this->user->email,
-        'phone' => $this->user->phone,
-      ],
-      'created_at' => $this->created_at,
-      'updated_at' => $this->updated_at,
-    ];
-
-    if ($this->order_type !== 'investor') {
-      $data['address'] = [
-        'id' => $this->orderAddress->address->id,
-        'street' => $this->orderAddress->address->street,
-        'city' => $this->orderAddress->address->city,
-        'state' => $this->orderAddress->address->state,
-        'country' => $this->orderAddress->address->country,
-        'postal_code' => $this->orderAddress->address->postal_code,
-        'building_number' => $this->orderAddress->address->building_number,
-        'mobile_number' => $this->orderAddress->address->mobile_number,
-        'latitude' => $this->orderAddress->address->latitude,
-        'longitude' => $this->orderAddress->address->longitude,
-      ];
+    /**
+     * Transform the resource into an array for the index API.
+     *
+     * @return array<string, mixed>
+     */
+    public function transformForIndex(): array
+    {
+        return [
+          'id' => $this->id,
+          'address' => [
+            'city' => $this->orderAddress->address->city,
+            'street' => $this->orderAddress->address->street,
+            'building_number' => $this->orderAddress->address->building_number,
+          ],
+          'user' => [
+            'name' => $this->user->name,
+          ],
+          'status' => $this->getStatusAttribute($this->order_status),
+          'created_at' => $this->created_at,
+          'total' => $this->total,
+        ];
     }
 
-    return $data;
-  }
+    /**
+     * Transform the resource into an array for the show API.
+     *
+     * @return array<string, mixed>
+     */
+    public function transformForShow(): array
+    {
+        $orderItems = $this->orderProducts->map(function ($orderProduct) {
+            $product = $orderProduct->product;
+            $discountedPrice = $orderProduct->price * (1 - ($product->discount / 100));
+            $totalPrice = $discountedPrice * $orderProduct->quantity;
+            return [
+              'id' => $orderProduct->id,
+              'product_name' => $product->title,
+              'product_price' => $orderProduct->price,
+              'quantity' => $orderProduct->quantity,
+              'discount' => $product->discount,
+              'total_price' => $totalPrice,
+              'media' => $product->getMedia('products')->map(function ($media) {
+                  return [
+                    'original' => $media->getUrl(),
+                    'thumb' => $media->getUrl('thumb'),
+                    'medium' => $media->getUrl('medium'),
+                    'large' => $media->getUrl('large'),
+                  ];
+              }),
+            ];
+        });
+        $data = [
+          'id' => $this->id,
+          'order_type' => $this->order_type,
+          'total' => $this->total,
+          'status' => $this->getStatusAttribute($this->order_status),
+          'order_products' => $orderItems,
+          'user' => [
+            'id' => $this->user->id,
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'phone' => $this->user->phone,
+          ],
+          'created_at' => $this->created_at,
+          'updated_at' => $this->updated_at,
+        ];
+
+        if ($this->order_type !== 'investor') {
+            $data['address'] = [
+              'id' => $this->orderAddress->address->id,
+              'street' => $this->orderAddress->address->street,
+              'city' => $this->orderAddress->address->city,
+              'state' => $this->orderAddress->address->state,
+              'country' => $this->orderAddress->address->country,
+              'postal_code' => $this->orderAddress->address->postal_code,
+              'building_number' => $this->orderAddress->address->building_number,
+              'mobile_number' => $this->orderAddress->address->mobile_number,
+              'latitude' => $this->orderAddress->address->latitude,
+              'longitude' => $this->orderAddress->address->longitude,
+            ];
+        }
+
+        return $data;
+    }
 }
