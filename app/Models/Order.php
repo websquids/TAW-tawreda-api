a41f7704dfd05c3f65a64852239db56fc145a94c
omar-abdelrady-pintar <omar.abdelrady@pintar.tech>
2025-01-28 02:34:36 +0200

    update 5 files

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,94 +5,88 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
-{
-    use HasFactory;
+class Order extends Model {
+  use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-      'user_id',
-      'order_type',
-      'order_status',
-      'total',
-      'status',
-    ];
-
-
-    public const ORDER_TYPES = [
-      'CUSTOMER' => 'customer',
-      'INVESTOR' => 'investor',
-    ];
-
-    public const ORDER_STATUSES = [
-      'PENDING' => 0,
-      'PROCESSING' => 1,
-      'COMPLETED' => 2,
-      'CANCELED' => 3,
-      'FAILED' => 5,
-      'REFUNDED' => 4,
-      'REFUND_PENDING' => 6,
-      'REFUND_FAILED' => 7,
-      'REFUND_COMPLETED' => 8,
-    ];
-
-    public function getStatusAttribute($value)
-    {
-        // dd($value);
-        return array_flip(self::ORDER_STATUSES)[$value];
-    }
-
-    protected static array $fields = [
-      'order_type' => [
-        'searchable' => true,
-        'sortable' => true,
-      ],
-      'order_status' => [
-        'searchable' => true,
-        'sortable' => true,
-      ],
-      'total' => [
-        'searchable' => true,
-        'sortable' => true,
-      ],
-      'status' => [
-        'searchable' => true,
-        'sortable' => true,
-      ],
-      'created_at' => [
-        'searchable' => false,
-        'sortable' => true,
-      ],
-      'updated_at' => [
-        'searchable' => false,
-        'sortable' => true,
-      ],
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'user_id',
+    'order_type',
+    'order_status',
+    'total',
+    'status',
+  ];
 
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+  public const ORDER_TYPES = [
+    'CUSTOMER' => 'customer',
+    'INVESTOR' => 'investor',
+  ];
 
-    public function orderAddress()
-    {
-        return $this->hasOne(OrderAddress::class, 'order_id', 'id');
-    }
+  public const ORDER_STATUSES = [
+    'PENDING' => 0,
+    'PROCESSING' => 1,
+    'COMPLETED' => 2,
+    'CANCELED' => 3,
+    'FAILED' => 5,
+    'REFUNDED' => 4,
+    'REFUND_PENDING' => 6,
+    'REFUND_FAILED' => 7,
+    'REFUND_COMPLETED' => 8,
+  ];
 
-    public function orderProducts()
-    {
-        return $this->hasMany(OrderProduct::class);
-    }
+  public function getStatusAttribute($value) {
+    // dd($value);
+    return array_flip(self::ORDER_STATUSES)[$value];
+  }
+
+  protected static array $fields = [
+    'order_type' => [
+      'searchable' => true,
+      'sortable' => true,
+    ],
+    'order_status' => [
+      'searchable' => true,
+      'sortable' => true,
+    ],
+    'total' => [
+      'searchable' => true,
+      'sortable' => true,
+    ],
+    'status' => [
+      'searchable' => true,
+      'sortable' => true,
+    ],
+    'created_at' => [
+      'searchable' => false,
+      'sortable' => true,
+    ],
+    'updated_at' => [
+      'searchable' => false,
+      'sortable' => true,
+    ],
+  ];
 
 
-    public static function getFields(): array
-    {
-        $fields = self::$fields;
-        return $fields;
-    }
+  public function user() {
+    return $this->belongsTo(User::class);
+  }
+
+  public function orderAddress() {
+    return $this->hasOne(OrderAddress::class, 'order_id', 'id');
+  }
+
+  public function orderProducts() {
+    return $this->hasMany(OrderProduct::class);
+  }
+
+
+  public static function getFields(): array {
+    $fields = self::$fields;
+    return $fields;
+  }
 }
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -7,58 +7,48 @@ use App\Http\Resources\Customer\OrderResource;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
-class OrderService
-{
-    protected OrderFilter $orderFilter;
-    public function __construct()
-    {
-        $this->orderFilter = new OrderFilter();
-    }
-    public function getFilteredOrders(Request $request)
-    {
-        $orders = $this->orderFilter->apply(Order::query(), $request);
+class OrderService {
+  protected OrderFilter $orderFilter;
+  public function __construct() {
+    $this->orderFilter = new OrderFilter();
+  }
+  public function getFilteredOrders(Request $request) {
+    $orders = $this->orderFilter->apply(Order::query(), $request);
 
-        $paginationOrders = $orders->paginate($request->get('perPage', 10));
+    $paginationOrders = $orders->paginate($request->get('perPage', 10));
 
-        $paginationOrders->data = OrderResource::collection($paginationOrders);
+    $paginationOrders->data = OrderResource::collection($paginationOrders);
 
-        return $paginationOrders;
-    }
+    return $paginationOrders;
+  }
 
-    public function create($orderData)
-    {
-        $order = Order::create($orderData);
-        return $order;
-    }
+  public function create($orderData) {
+    $order = Order::create($orderData);
+    return $order;
+  }
 
-    public function getOrderById($id)
-    {
-        $order = Order::find($id)->get();
-        return OrderResource::collection($order);
-    }
+  public function getOrderById($id) {
+    $order = Order::find($id)->get();
+    return OrderResource::collection($order);
+  }
 
-    public function updateOrder($request, $order)
-    {
-        $order->update($request->validated());
-        $order->save();
-        return $order;
-    }
+  public function updateOrder($request, $order) {
+    $order->update($request->validated());
+    $order->save();
+    return $order;
+  }
 
-    public function deleteOrder($order)
-    {
-        $order->delete();
-    }
+  public function deleteOrder($order) {
+    $order->delete();
+  }
 
-    public function getAllOrderStatus()
-    {
-        return Order::ORDER_STATUSES;
-    }
+  public function getAllOrderStatus() {
+    return Order::ORDER_STATUSES;
+  }
 
-    public function updateOrderStatus($order, $status)
-    {
-
-        $order->update(['order_status' => $status]);
-        $order->save();
-        return $order;
-    }
+  public function updateOrderStatus($order, $status) {
+    $order->update(['order_status' => $status]);
+    $order->save();
+    return $order;
+  }
 }
